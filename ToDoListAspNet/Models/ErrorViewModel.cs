a62c887fe361986c2e8a242101a667d14bef6932
AlexanderLukashuk <upiter465@gmail.com>
2023-05-16 14:36:31 +0600

    First creation

---
+++ ToDoListAspNet/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace ToDoListAspNet.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
