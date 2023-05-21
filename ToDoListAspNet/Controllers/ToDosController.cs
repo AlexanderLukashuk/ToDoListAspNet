8ad82140c364c4f4cce65047de51e958c5154bf6
AlexanderLukashuk <upiter465@gmail.com>
2023-05-21 21:45:02 +0600

    Created ToDoService class

--- ToDoListAspNet/Controllers/ToDosController.cs
+++ ToDoListAspNet/Controllers/ToDosController.cs
@@ -76,46 +76,7 @@ namespace ToDoListAspNet.Controllers
             //    }
             //}
 
-            try
-            {
-                var connection = new SqlConnection(connectionString);
-
-                //using (var connection = new SqlConnection(connectionString))
-                using (connection)
-                {
-                    try
-                    {
-                        connection.Open();
-
-                        string query =
-                            "INSERT INTO ToDos (Name, Description, DeadLine, ToDoStatus)" +
-                            "VALUES (@name, @descr, CAST(@deadLine AS DateTime), @status);";
-
-                        using (var command = new SqlCommand(query, connection))
-                        {
-                            command.Parameters.AddWithValue("@name", todo.Name);
-                            command.Parameters.AddWithValue("@descr", todo.Description);
-                            command.Parameters.AddWithValue("@deadLine", todo.DeadLine);
-                            command.Parameters.AddWithValue("@status", todo.ToDoStatus);
-                            //command.Parameters.AddWithValue("@deadLine", todo.DeadLine.ToString("yyyy-MM-dd HH:mm:ss"));
-
-                            command.ExecuteNonQuery();
-                        }
-                    }
-                    catch (SqlException ex)
-                    {
-                        Console.WriteLine("ERROR: Can't connect to server" + ex.Message);
-                    }
-                    finally
-                    {
-                        connection.Close();
-                    }
-                }
-            }
-            catch (SqlException ex)
-            {
-                Console.WriteLine("ERROR: Something went wrong" + ex.Message);
-            }
+            
             return RedirectToAction(nameof(Index));
         }
 
---
+++ ToDoListAspNetLibrary/Services/ToDoService.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Data.SqlClient;
+using ToDoListAspNetLibrary.Models.Entities;
+
+namespace ToDoListAspNetLibrary.Services
+{
+	public class ToDoService
+	{
+		public void Create(ToDo todo, string connectionString)
+		{
+            try
+            {
+                //using (var connection = new SqlConnection(connectionString))
+                using (var connection = new SqlConnection(connectionString))
+                {
+                    try
+                    {
+                        connection.Open();
+
+                        string query =
+                            "INSERT INTO ToDos (Name, Description, DeadLine, ToDoStatus)" +
+                            "VALUES (@name, @descr, CAST(@deadLine AS DateTime), @status);";
+
+                        using (var command = new SqlCommand(query, connection))
+                        {
+                            command.Parameters.AddWithValue("@name", todo.Name);
+                            command.Parameters.AddWithValue("@descr", todo.Description);
+                            command.Parameters.AddWithValue("@deadLine", todo.DeadLine);
+                            command.Parameters.AddWithValue("@status", todo.ToDoStatus);
+                            //command.Parameters.AddWithValue("@deadLine", todo.DeadLine.ToString("yyyy-MM-dd HH:mm:ss"));
+
+                            command.ExecuteNonQuery();
+                        }
+                    }
+                    catch (SqlException ex)
+                    {
+                        Console.WriteLine("ERROR: Can't connect to server" + ex.Message);
+                    }
+                    finally
+                    {
+                        connection.Close();
+                    }
+                }
+            }
+            catch (SqlException ex)
+            {
+                Console.WriteLine("ERROR: Something went wrong" + ex.Message);
+            }
+        }
+	}
+}
+
