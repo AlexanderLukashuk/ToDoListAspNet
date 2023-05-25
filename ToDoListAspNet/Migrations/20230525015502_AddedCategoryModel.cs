33130a4aeca58f60ad543a62cd0e84e56eacc2e2
AlexanderLukashuk <upiter465@gmail.com>
2023-05-25 07:56:27 +0600

    Created Model Category, added new migrations, added property CategoryId in ToDo

---
+++ ToDoListAspNet/Migrations/20230525015502_AddedCategoryModel.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ToDoListAspNet.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedCategoryModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "CategoryId",
+                table: "ToDos",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateTable(
+                name: "Category",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Progress = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Category", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ToDos_CategoryId",
+                table: "ToDos",
+                column: "CategoryId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ToDos_Category_CategoryId",
+                table: "ToDos",
+                column: "CategoryId",
+                principalTable: "Category",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ToDos_Category_CategoryId",
+                table: "ToDos");
+
+            migrationBuilder.DropTable(
+                name: "Category");
+
+            migrationBuilder.DropIndex(
+                name: "IX_ToDos_CategoryId",
+                table: "ToDos");
+
+            migrationBuilder.DropColumn(
+                name: "CategoryId",
+                table: "ToDos");
+        }
+    }
+}
